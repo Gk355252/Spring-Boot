39e6102a74430ead0b27c648abac6ee350b893a9
Govind Kumar <103330862+Gk355252@users.noreply.github.com>
2024-07-02 15:02:22 +0530

    Just Testing

---
+++ src/main/java/com/codingshuttle/anuj/week1Introduction/IntroductionToSpringBoott/IntroductionToSpringBoottApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.anuj.week1Introduction.IntroductionToSpringBoott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBoottApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBoottApplication.class, args);
+	}
+
+}
